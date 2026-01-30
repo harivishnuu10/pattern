e2d1c2a9de7b461a513818cd4f534947c1abd673
Harivishnu <harivishnuu10@gmail.com>
2026-01-30 19:24:13 +0530

    Add Triangle Wave Pattern Java program

---
+++ Triangle Wave Pattern.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class solution
+{
+  public static void main(String args[])
+  {
+    Scanner scan=new Scanner(System.in);
+    System.out.println("Enter Number:  ");
+    int n=scan.nextInt();
+    for(int i=0;i<n;i++)
+    {  
+      for(int j=0;j<=i;j++)
+      {
+        System.out.print("*");
+      }
+    System.out.println();
+    }
+    for(int i=0;i<n-1;i++)
+    {  
+      for(int j=0;j<n-i-1;j++)
+      {
+        System.out.print("*");
+      }
+    System.out.println();
+    }
+}
+}
